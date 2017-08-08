b1d9405c320bba6138e57e4c79422192c0308bf6
anla7856 <anla7856@anLA7856>
2017-08-08 22:23:33 +0800

    增加注解的方式来描述sql

--- src/main/java/study/helloworld/mapper/RoleMapper.java
+++ src/main/java/study/helloworld/mapper/RoleMapper.java
@@ -1,8 +1,11 @@
 package study.helloworld.mapper;
 
+import org.apache.ibatis.annotations.Select;
+
 import study.helloworld.po.Role;
 
 public interface RoleMapper {
+	@Select(value="select id, role_name as roleName, note from t_role where id = #{id}")
 	public Role getRole(Long id);
 	public int insertRole(Role role);
 	public int deleteRole(Long id);
