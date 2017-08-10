0ad4af69c2bc7c6f53797f31643583b45e39d4cf
anla7856 <anla7856@anLA7856>
2017-08-10 23:21:55 +0800

    添加cglib的测试方法
    Signed-off-by: anla7856 <anla7856@anLA7856>

--- src/test/java/study/JDKProxy/test/TestJDKProxy.java
+++ src/test/java/study/JDKProxy/test/TestJDKProxy.java
@@ -1,5 +1,8 @@
 package study.JDKProxy.test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.junit.Test;
 
 import study.JDKProxy.HelloService;
@@ -13,4 +16,16 @@ public class TestJDKProxy {
 		HelloService proxy = (HelloService) helloHandler.bind(new HelloServiceImpl());
 		proxy.sayHello("Tom");
 	}
+	
+	@Test
+	public void testCgLibProxy() throws InstantiationException, IllegalAccessException, ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException {
+		Object service = Class.forName(HelloServiceImpl.class.getName()).newInstance();
+		//获取服务方法hello
+		Method method = service.getClass().getMethod("sayHello", String.class);
+		method.invoke(service, "June");
+	}
 }
+
+
+
+
